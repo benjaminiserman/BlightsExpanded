ef99614d2c980bc1bd9ff8d83501ebd31b277eff
winggar <winggar1228@gmail.com>
2025-06-29 00:10:52 -0700

    fix some things, but still not quite working

--- Source/BlightsExpanded/Blights/BoomblightBlight.cs
+++ Source/BlightsExpanded/Blights/BoomblightBlight.cs
@@ -6,6 +6,27 @@ namespace BlightsExpanded.Blights
 {
     public class BoomblightBlight : CustomBlight
     {
+        public ThingComp ExplosiveComp = new CompExplosive();
+
         public override int AdjustYieldAmount(int yield) => (int)Math.Round(yield * 1.2);
+
+        public override void PreApplyDamage(ref DamageInfo dinfo, out bool absorbed)
+        {
+            if (dinfo.Def == DamageDefOf.Flame)
+            {
+                GenExplosion.NotifyNearbyPawnsOfDangerousExplosive(this, DamageDefOf.Flame);
+                GenExplosion.DoExplosion(
+                    Position,
+                    Map,
+                    1.5f,
+                    DamageDefOf.Flame,
+                    null,
+                    preExplosionSpawnThingDef: ThingDefOf.Filth_Fuel,
+                    preExplosionSpawnChance: 1f
+                );
+            }
+
+            absorbed = false;
+        }
     }
 }
